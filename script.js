bab1cbd5840a44c12374cfb98176041cffa1072d
Klemens Kraft <klemenskraft@gmail.com>
2024-02-18 21:43:03 +0100

    Write the constructor function and a function to add new books to the array

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let test = "Hello";
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+};
+
+function addBookToLibrary() {
+  let title = prompt("What's the title of the book?");
+  let author = prompt("Who is the author of the book?");
+  let pages = prompt("How many pages does the book have?");
+  let read = prompt("Have you read the book?")
+  myLibrary.push(new Book(title, author, pages, read));
+};
